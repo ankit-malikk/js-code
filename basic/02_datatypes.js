5f9e32eb6e5b92baf5af1e90503022f3d04ab5b5
ankit-malikk <malikankit447@gmail.com>
2026-03-06 18:00:01 +0000

    datatypes

---
+++ basic/02_datatypes.js
@@ -0,0 +1,29 @@
+ "use strict"; /*  treat all js code as newer version*/
+
+//  alert( 3 + 3) we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3) //code readability should be high
+
+    console.log("ankit");
+    let name ="ankit"// string
+    let age = 22 //number
+    let isLoggedIn = false //bollean
+
+    //number = 2 to power 53
+    //bigint
+    //string = " "
+    //bolean = true/false
+    //null = standalone value
+    //undefined =
+    //symbol = unique
+
+    //object
+    console.log(typeof null);//object
+    console.log(typeof undefined); //undefined
+    
+    
+
+    
+

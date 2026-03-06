c29e8e792156c9d254760f468ee404fb0065a281
ankit-malikk <malikankit447@gmail.com>
2026-03-06 19:05:55 +0000

    comparison

---
+++ basic/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 => 1);
+// console.log(2 > 1);
+// console.log(2 == 1);
+
+console.log("2" > 1); 
+console.log("02" > 1);//when we compare must datatype same
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);//must avoid
+
+
+// === strictcheck
+
+console.log("2" === 2);
+
+
+
+

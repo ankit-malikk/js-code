0c50855b98885674b9b77efaed1a66d78ab79f6e
ankit-malikk <malikankit447@gmail.com>
2026-03-07 17:09:34 +0000

    mathandnum

---
+++ basic/06_nums_maths.js
@@ -0,0 +1,32 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));//after decial to fixed
+
+const otherNumber = 123.8966
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));//en-In for indian number 
+
+
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+
+console.log(Math.min(4 , 3, 1, 7));
+console.log(Math.max(3,5,6,7));
+
+console.log((Math.random()*10) +1);//math random give value 0 and 1
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min +1)) + 10)
+
+
+
